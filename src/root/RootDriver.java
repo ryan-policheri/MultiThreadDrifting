598123d45cb5738ee32e93608f05865b7ea0ea6d
Ryan Policheri <Ryan-Poli90@outlook.com>
2021-09-25 19:33:45 -0500

    small change

--- src/root/RootDriver.java
+++ src/root/RootDriver.java
@@ -6,8 +6,11 @@ import common.BinaryFileToTextFile;
 import common.DataSetGenerator;
 import common.ISortFile;
 import mapreduce.MapReduceProcessor;
+import quicksort.QuickSortProcessor;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 public class RootDriver {
     public static String BASELINE = "BASELINE";
@@ -15,19 +18,26 @@ public class RootDriver {
     public static String MAP_REDUCE = "MAP_REDUCE";
     public static String QUICK_SORT = "QUICK_SORT";
 
-    public static void main(String[] args) throws IOException { //Main driver that can all our solutions and serve as a utility for generating files
+    public static void main(String[] args) throws IOException, InterruptedException { //Main driver that can all our solutions and serve as a utility for generating files
         if (args[0].equals("--GenerateFile")) generateFile(args); //user called the utility for generating files
         else { //normal input to call our solutions
             String inputFilePath = args[0].replace("\"", "");
             String outputFilePath = args[1].replace("\"", "");
             int numberOfThreads = Integer.parseInt(args[2]);
             String solution = args[3].toUpperCase();
-            int inputSize = Integer.parseInt(args[1]);
 
             ISortFile processor;
             if (solution.equals(BASELINE)) processor = new BaseLineSortProcessor();
             else if (solution.equals(BUCKET_SORT)) processor = new BucketSort1Processor(100, numberOfThreads); //we don't know what a good bucket length is ahead of time, that's the problem w/ bucket sort
             else if (solution.equals(MAP_REDUCE)) processor = new MapReduceProcessor(numberOfThreads);
+            else if (solution.equals(QUICK_SORT)) processor = new QuickSortProcessor(numberOfThreads);
+            else throw new IllegalArgumentException("Solution name not known");
+
+            Instant totalStartTime = Instant.now();
+            processor.sortFile(inputFilePath, outputFilePath);
+            Instant totalEndTime = Instant.now();
+            long totalTimeTaken = Duration.between(totalStartTime, totalEndTime).toMillis();
+            System.out.printf("Total time taken for " + solution + " with " + numberOfThreads + " threads" + ": %dms\n", totalTimeTaken);
         }
     }
 
